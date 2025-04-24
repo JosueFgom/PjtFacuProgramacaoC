ab7025ab77c2650e98b52524408df15c3650e8fa
Josué Ferreira <110063274+JosueFgom@users.noreply.github.com>
2025-04-24 04:42:08 +0000

    Primeiro Commit de dentro do VSCODE

---
+++ n1_nc4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Declarando as primeiras variáveis em C
+
+int main () {
+
+    int idade;
+    int quantidade;
+    char nome[20];
+    float altura;
+    char letra;
+    int _teste;
+    int teste_2;
+    double peso;
+
+
+     
+
+
+}
